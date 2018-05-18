f5a33134958f167edf7a0eee148f4cd4d16b6bab
Amber Caldwell <acaldwell710@gmail.com>
2018-05-18 14:24:28 -0400

    added sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,7 +84,7 @@ Rails.application.configure do
   }
 
  # ActionMailer Config
- config.action_mailer.default_url_options = { :host => Rails.application.secrets.domain_name }
+ config.action_mailer.default_url_options = { :host => host: 'blocmetrics-amber-caldwell.herokuapp.com' }
  config.action_mailer.delivery_method = :smtp
  config.action_mailer.perform_deliveries = true
  config.action_mailer.raise_delivery_errors = true
